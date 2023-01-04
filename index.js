3a6ece543270f435ada77e9c2291f08114b3ab9d
ori_b <ori@ori>
2023-01-04 08:15:53 +0200

    adding files

---
+++ index.js
@@ -0,0 +1,10 @@
+function func(e) {
+elem = document.body;//.style.backgroundColor;
+	if (elem.style.backgroundColor == "black"){
+		elem.style.backgroundColor = "white";
+	}else{
+		elem.style.backgroundColor = "black";
+
+	}
+
+}
